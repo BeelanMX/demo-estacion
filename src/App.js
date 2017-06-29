c23b7de577175413ed50daede9b0e533f19c4570
Alejandro Medina <5almero5@gmail.com>
2017-06-29 14:40:27 -0500

    add circleComponent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CircleChart from './components/CircleChart'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data1: 30,
+      data2: 45.5
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Beelan</h2>
+          <h2>Welcome to Beelan Demo</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="chartsSections">
+        <CircleChart refs='one' title='ONE' data={this.state.data1}/>
+        <CircleChart refs='two' title='TwO' data={this.state.data2}/>
+        </div>
       </div>
     );
   }
