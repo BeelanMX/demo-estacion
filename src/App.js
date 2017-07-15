647902a37c709bea001d6a433dc7e11113fd2780
Alejandro Medina <5almero5@gmail.com>
2017-07-15 15:44:43 -0500

    rename title

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Beelan Demo</h2>
+          <h2>Beelan Demo Estación Meteorológica</h2>
         </div>
         <div className="chartsSections">
           {
